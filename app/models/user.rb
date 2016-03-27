097ce324ebac7193c9f83628924b5234dbcedf45
Brad van der Laan <brad.vanderlaan@gmail.com>
2016-03-26 21:24:30 -0400

    Add validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
   has_secure_password
+  before_save { self.email = email.downcase }
+  validates :first_name,
+            presence: true
+  validates :last_name,
+            presence: true
+  validates :user_name, 
+  			presence: true,
+            length: { maximum: 72 },
+            uniqueness: { case_sensitive: false }
+  validates :email, 
+  			presence: true,
+            length: { maximum: 255 }, 
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+            uniqueness: { case_sensitive: false }
+  
 end
