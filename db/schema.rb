6dc9cf6f9815e511a9677e68f9b4b8e6c05ee5da
Brad van der Laan <brad.vanderlaan@gmail.com>
2016-03-26 21:15:37 -0400

    Update schema to include a Users table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160321004027) do
+ActiveRecord::Schema.define(version: 20160327010638) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title",         limit: 255
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20160321004027) do
     t.string   "location",      limit: 255
     t.datetime "startdatetime"
     t.integer  "duration",      limit: 4
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name",      limit: 255
+    t.string   "last_name",       limit: 255
+    t.string   "user_name",       limit: 255
+    t.string   "email",           limit: 255
+    t.string   "password_digest", limit: 255
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
   end
