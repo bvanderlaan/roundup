d741dcd144fcb9aefa30e4eabff280d97042897b
Brad van der Laan <brad.vanderlaan@gmail.com>
2016-03-27 11:48:40 -0400

    Add association between the Users table and the Events table via the command 'rails g migration add_user_to_events user:references'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160327010638) do
+ActiveRecord::Schema.define(version: 20160327154716) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title",         limit: 255
@@ -21,7 +21,10 @@ ActiveRecord::Schema.define(version: 20160327010638) do
     t.integer  "duration",      limit: 4
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.integer  "user_id",       limit: 4
   end
+
+  add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",      limit: 255
@@ -33,4 +36,5 @@ ActiveRecord::Schema.define(version: 20160327010638) do
     t.datetime "updated_at",                  null: false
   end
 
+  add_foreign_key "events", "users"
 end
